5a897a8f3349a4d799a81ddda882cfbb511997fd
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-14 19:19:13 +0300

    Adding getter and setter methods in Book class

--- src/enity/Book.java
+++ src/enity/Book.java
@@ -6,9 +6,9 @@ public class Book {
     private int pagesNumber;
     private String userName;
     private String userEmail;
-    private String userID;
+    private int userID;
 
-    public Book (String bookName, String author, int pagesNumber, String userName, String userEmail, String userID){
+    public Book (String bookName, String author, int pagesNumber, String userName, String userEmail, int userID){
         this.bookName = bookName;
         this.author = author;
         this.pagesNumber = pagesNumber;
@@ -16,4 +16,59 @@ public class Book {
         this.userEmail = userEmail;
         this.userID = userID;
     }
+
+    public String getBookName(){
+        return  bookName;
+    }
+
+    public void setBookName(String bookName){
+        this.bookName = bookName;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public  void setAuthor(String author){
+        this.author = author;
+    }
+
+    public int getPagesNumber(){
+        return pagesNumber;
+    }
+
+    public void setPagesNumber(int pagesNumber){
+        this.pagesNumber = pagesNumber;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getUserEmail(){
+        return  userEmail;
+    }
+
+    public void setUserEmail(String userEmail){
+        this.userEmail = userEmail;
+    }
+
+    public int getUserID(){
+        return userID;
+    }
+
+    public void setUserID(int userID){
+        this.userID = userID;
+    }
+
+
+
+
+
+
+
+
 }

dcb8d173eafa0992916392dcb7cc5d5d77d59d48
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-14 19:09:41 +0300

    Adding a constructor to a book class

--- src/enity/Book.java
+++ src/enity/Book.java
@@ -1,4 +1,19 @@
 package enity;
 
 public class Book {
+    private String bookName;
+    private String author;
+    private int pagesNumber;
+    private String userName;
+    private String userEmail;
+    private String userID;
+
+    public Book (String bookName, String author, int pagesNumber, String userName, String userEmail, String userID){
+        this.bookName = bookName;
+        this.author = author;
+        this.pagesNumber = pagesNumber;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userID = userID;
+    }
 }

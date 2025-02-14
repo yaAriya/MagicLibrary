5a2e1a212b93b573a31d0e294b8532ab56538024
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-14 19:25:07 +0300

    Adding a constructor to a User class

--- src/enity/User.java
+++ src/enity/User.java
@@ -1,4 +1,15 @@
 package enity;
 
 public class User {
+    private String userName;
+    private String userEmail;
+    private int userAge;
+    private int userID;
+
+    public User (String userName, String userEmail, int userAge, int userID){
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userAge = userAge;
+        this.userID = userID;
+    }
 }

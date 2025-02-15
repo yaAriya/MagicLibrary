6ad4bf3e3405f042e2231aa2d47bbe5841b429c2
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-15 18:31:48 +0300

    Adding ArrayLists with objects in DAO classes

--- src/DAOClass/BookDAO.java
+++ src/DAOClass/BookDAO.java
@@ -1,4 +1,29 @@
 package DAOClass;
 
+import enity.Book;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class BookDAO {
+    public List<Book> initializeIssuedBooks(){
+        List<Book> issuedBooks = new ArrayList<>();
+
+        Book firstBook = new Book("Pride and produce", "Jane Austen", 352, "Elsa", "Elisa@gmail.com", 245);
+        Book secondBook = new Book("Harry Potter", "J. K. Rowling", 302, "Petr", "Petya@gmail.com", 356);
+        Book thirdBook = new Book("The Mysterious Island", "Jules Verne", 597, "Nick", "NickNick@gmail.com", 134);
+
+        issuedBooks.add(firstBook);
+        issuedBooks.add(secondBook);
+        issuedBooks.add(thirdBook);
+
+        return issuedBooks;
+    }
+
+    public List<Book> initializeBooksOnShelves(){
+        List<Book> booksOnShelves = new ArrayList<>();
+
+
+    }
 }
--- src/DAOClass/UserDAO.java
+++ src/DAOClass/UserDAO.java
@@ -1,4 +1,38 @@
 package DAOClass;
 
+import enity.User;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class UserDAO {
+    public List<User> initializerCurrentUser(){
+        List <User> currentUsers = new ArrayList<>();
+
+        User firstUser = new  User("Nick", "NickNick@gmail.com", 14, 134);
+        User secondUser = new User("Elsa", "Elisa@gmail.com", 16, 245);
+        User thirdUser = new User("Petr", "Petya@gmail.com", 10, 356);
+
+        currentUsers.add(firstUser);
+        currentUsers.add(secondUser);
+        currentUsers.add(thirdUser);
+
+        return currentUsers;
+    }
+
+    public List<User> initializerExpiredUsers(){
+        List <User> expiredUsers = new ArrayList<>();
+
+        User firstUser = new User("Lisa", "Lisochka@gmail.com", 11, 467);
+        User secondUser = new User("Pasha", "Pashka@gmail.com", 19, 578);
+        User thirdUser = new User("Vladimir", "Vovchik@gmail.com", 15, 689);
+
+        expiredUsers.add(firstUser);
+        expiredUsers.add(secondUser);
+        expiredUsers.add(thirdUser);
+
+    return expiredUsers;
+    }
+
 }

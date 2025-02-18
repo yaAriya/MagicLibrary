98fbb1822786b365f1c86a3d6eb07395673da0fe
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-18 18:50:23 +0300

    Changing the methods of DAO classes

--- src/DAOClass/DAOBook.java
+++ src/DAOClass/DAOBook.java
@@ -5,11 +5,10 @@ import enity.Book;
 import java.util.List;
 
 public interface DAOBook {
-    List<Book> getAllBooks();
     void add(Book book);
     Book upDate(final Book book);
     void delete(Book book);
-    Book getByID(int ID);
+    Book read(int ID);
 
 
 }
--- src/DAOClass/DAOUser.java
+++ src/DAOClass/DAOUser.java
@@ -5,9 +5,8 @@ import enity.User;
 import java.util.List;
 
 public interface DAOUser {
-    List<User> getAllUsers();
     void add(User user);
     User upDate(User user);
     void delete(User user);
-    User getByID(int ID);
+    User read(int ID);
 }

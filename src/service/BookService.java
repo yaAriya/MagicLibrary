6785fc65e299c557b2d1c2d7754e87a9ec419b72
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-23 09:32:12 +0300

    Adding implementation of CRUD methods in BookService class

--- src/service/BookService.java
+++ src/service/BookService.java
@@ -1,4 +1,26 @@
 package service;
 
-public class BookService {
+import enity.Book;
+
+public class BookService implements ServiceForBook{
+
+
+    @Override
+    public void add(Book book) {
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book read(int ID) {
+        return null;
+    }
+
+    @Override
+    public void delete(Book book) {
+
+    }
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,4 +1,25 @@
 package service;
 
-public class UserService {
+import enity.User;
+
+public class UserService implements ServiceForUser{
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public User upDate(User user) {
+        return null;
+    }
+
+    @Override
+    public User read(int ID) {
+        return null;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
 }

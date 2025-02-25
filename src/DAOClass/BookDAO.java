ada1a4c1c996e84f6edb3bc646a565defd8b997b
Yarochkina Ariya <yarochkina.ariya@gmail.com>
2025-02-25 18:02:32 +0300

    deleting objects from the users and books ArrayLists

--- src/DAOClass/BookDAO.java
+++ src/DAOClass/BookDAO.java
@@ -10,19 +10,6 @@ public class BookDAO implements DAOBook {
 
     public static List<Book> initializeBooks() {
         List<Book> books = new ArrayList<>();
-
-        Book firstBook = new Book("Pride and produce", "Jane Austen", 352, 145, null);
-                //352, "Elsa", "Elisa@gmail.com", 245);
-        Book secondBook = new Book("Harry Potter", "J. K. Rowling", 302, 326, null);// Вызываем дао юзер а тот вызовет юзера
-                //"Petr", "Petya@gmail.com", 356);
-        Book thirdBook = new Book("The Mysterious Island", "Jules Verne", 597, 467, null);
-                //"Nick", "NickNick@gmail.com", 134);
-
-        books.add(firstBook);
-        books.add(secondBook);
-        books.add(thirdBook);
-
-
         return books;
     }
 
--- src/DAOClass/UserDAO.java
+++ src/DAOClass/UserDAO.java
@@ -10,21 +10,6 @@ public class UserDAO implements DAOUser{
     private static List<User> initializeUsers(){
         List<User> users = new ArrayList<>();
 
-        User firstUser = new  User("Nick", "NickNick@gmail.com", 14, 134, null);
-        User secondUser = new User("Elsa", "Elisa@gmail.com", 16, 245, null);
-        User thirdUser = new User("Petr", "Petya@gmail.com", 10, 356, null);
-        User fourthUser = new User("Lisa", "Lisochka@gmail.com", 11, 467, null);
-        User fifthUser = new User("Pasha", "Pashka@gmail.com", 19, 578, null);
-        User sixthUser = new User("Vladimir", "Vovchik@gmail.com", 15, 689, null);
-
-        users.add(firstUser);
-        users.add(secondUser);
-        users.add(thirdUser);
-        users.add(fourthUser);
-        users.add(fifthUser);
-        users.add(sixthUser);
-
-
         return users;
     }
 
